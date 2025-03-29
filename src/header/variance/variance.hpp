6338593c9d9820b07e5d3a9d9cf820e7bf471b5a
susTuna <hidasetsuna@gmail.com>
2025-03-29 18:11:37 +0700

    feat: error calculation variance method header file

---
+++ src/header/variance/variance.hpp
@@ -0,0 +1,14 @@
+/* Variance Method Error Calculation Interface Header */
+#ifndef VARIANCE_HPP
+#define VARIANCE_HPP
+
+#include "errormethod.hpp"
+
+using namespace std;
+
+class Variance : public ErrorMethod{
+    public:
+        virtual double calculateError(const cv::Mat& block) const override;
+};
+
+#endif //VARIANCE_HPP
