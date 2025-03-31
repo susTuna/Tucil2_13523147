c1815c839f2812dac1a49a6ab11a49ff468c98d9
susTuna <hidasetsuna@gmail.com>
2025-03-31 14:21:23 +0700

    feat: ascii generator implementation

---
+++ src/utils/ascii/ascii.cpp
@@ -0,0 +1,12 @@
+#include "ascii/ascii.hpp"
+
+Ascii::Ascii(){}
+
+Ascii::~Ascii(){}
+
+void Ascii::displayAscii() const{
+    cout << "================================================================\n";
+    cout << "                           YUUKAPRESS                           \n";
+    cout << "================================================================\n";
+    cout << yuuka;
+}
