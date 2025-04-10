a9b5ac8bc4e1637f675048eaf0a15a6a90e31d86
susTuna <hidasetsuna@gmail.com>
2025-04-10 20:15:54 +0700

    chore: modified variance method to fit the formula

--- src/methods/variance.cpp
+++ src/methods/variance.cpp
@@ -1,13 +1,14 @@
 #include "variance/variance.hpp"
 
 double Variance::calculateError(FIBITMAP* image, int x, int y, int width, int height) const {
-    double sumR = 0, sumG = 0, sumB = 0, meanR = 0, meanG = 0, meanB = 0, varR = 0, varG = 0, varB = 0;
+    double sumR = 0, sumG = 0, sumB = 0, sumsqR = 0, sumsqG = 0, sumsqB = 0, meanR = 0, meanG = 0, meanB = 0, varR = 0, varG = 0, varB = 0;
     int pixels = width * height;
 
     for (int i = 0; i < height; ++i){
         for(int j = 0; j < width ; ++j){
             RGBQUAD color;
             if (FreeImage_GetPixelColor(image, x + j, y + i, &color)){
+                sumsqR += color.rgbRed * color.rgbRed, sumsqG += color.rgbGreen * color.rgbGreen, sumsqB += color.rgbBlue * color.rgbBlue;
                 sumR += color.rgbRed, sumG += color.rgbGreen, sumB += color.rgbBlue;
             }
         }
@@ -15,9 +16,9 @@ double Variance::calculateError(FIBITMAP* image, int x, int y, int width, int he
 
     meanR = sumR / pixels, meanG = sumG / pixels, meanB = sumB / pixels;
     
-    varR = (sumR * sumR) / pixels - meanR * meanR;
-    varG = (sumG * sumG) / pixels - meanG * meanG;
-    varB = (sumB * sumB) / pixels - meanB * meanB;
+    varR = sumsqR / pixels - meanR * meanR;
+    varG = sumsqG / pixels - meanG * meanG;
+    varB = sumsqB / pixels - meanB * meanB;
     
     /* σ_𝑅𝐺𝐵^2 = (σ_𝑅^2 + σ_𝐺^2 + σ_𝐵^2) / 3 */
     return (varR + varG + varB) / 3.0;
